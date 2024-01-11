e76d3f2a8e4dbc40d4814126d61822755da47cf3
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-11 16:37:27 +0530

    script second

--- 3-ifs.sh
+++ 3-ifs.sh
@@ -14,7 +14,7 @@ fi
 while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
     echo "username: $username"
-    echo "user ID: $user_id
+    echo "user ID: $user_id"
     echo "User Full name: $user_fullname"
 done < $file
 
