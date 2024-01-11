d11e37bfffb57c738b605b399b1dfa57deef1c19
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-12 00:20:37 +0530

    script second

--- 4-disk-usage.sh
+++ 4-disk-usage.sh
@@ -17,7 +17,12 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
+echo "$message" | mail -s "High Disk Usage" sridharreddy2508@gmail.com 
+
 #Message: High Disk Usage on /dev/xvdf: 2 --> this is the output came before addding + in 13 line
 #after adding + symbol, it will overwrite 2nd time loop and output is Message: High Disk Usage on /dev/xvda1: 30High Disk Usage on /dev/xvdf: 2
 #added \n new line character in 13 line and -e in 18th line
 #When we give special charecters like \n , we need to give –e it means to enable that special character given
+#output is below 2 lines:
+#Message: High Disk Usage on /dev/xvda1: 30
+#High Disk Usage on /dev/xvdf: 2
