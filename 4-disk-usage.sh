282f7d04c972e215d486d0674dcf210a9e9adc89
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-11 22:55:18 +0530

    script second

--- 4-disk-usage.sh
+++ 4-disk-usage.sh
@@ -10,12 +10,14 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage"
+        message+="High Disk Usage on $partition: $usage\n"
     fi
 
 done <<< $DISK_USAGE
 
-echo "Message: $message"
+echo -e "Message: $message"
 
 #Message: High Disk Usage on /dev/xvdf: 2 --> this is the output came before addding + in 13 line
-#after adding + symbol, it will overwrite 2nd time loop
+#after adding + symbol, it will overwrite 2nd time loop and output is Message: High Disk Usage on /dev/xvda1: 30High Disk Usage on /dev/xvdf: 2
+#added \n new line character in 13 line and -e in 18th line
+#When we give special charecters like \n , we need to give –e it means to enable that special character given
