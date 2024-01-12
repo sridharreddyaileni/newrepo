2f209bdb6537db80256ff0d39f7db288da9988e4
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-12 16:40:27 +0530

    script changes

--- 5-greetings.sh
+++ 5-greetings.sh
@@ -11,7 +11,7 @@ USAGE(){
     echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
     echo " -n, Specify the name (mandatory)"
-    echo " -w, Specify the wishes. ex, Good Morning"
+    echo " -w, Specify the wishes. (optional). Default is Good Morning"
     echo " -h, Display Help and exit"
 
 }
@@ -21,13 +21,15 @@ while getopts ":n:w:h" opt; do
         n) NAME="$OPTARG";;
         W) WISHES="$OPTARG";;
         \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
-        h) USAGE; exit;;
         :) USAGE; exit;;
+        h) USAGE; exit;;
     esac
 done
 
-if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-    echo "ERROR: Both -n and -w are mandatory options"
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+if [ -z "$NAME" ]; then #now wishes is optional
+    #echo "ERROR: Both -n and -w are mandatory options."
+    echo "ERROR: -n is mandatory,"
     USAGE
     exit 1
 fi
