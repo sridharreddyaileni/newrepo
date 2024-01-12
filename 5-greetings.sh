7dfd0eb8ca7a65b9380eef5e17a9fd625179002e
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-12 14:47:18 +0530

    script changes

--- 5-greetings.sh
+++ 5-greetings.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-PERSON=$1
+#PERSON=$1
 
-echo "Hello $PERSON Good Morning. We are learning shell script"
+#echo "Hello $PERSON Good Morning. We are learning shell script"
+
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. ex, Good Morning"
+    echo " -h, Display Help and exit"
+
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+        n) NAME="$OPTARG";;
+        W) WISHES="$OPTARG";;
+        h|*) USAGE; exit;;
+    esac
+done
